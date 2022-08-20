33163df76275dbce028abd2291d9a79e60a7d308
Enrique Kessler Martínez <enrikes@amazon.com>
2022-08-20 11:54:04 +0200

    Add working transaction printing
    We now have the correct dates being printed out. The config file feature
    is still non-implemented, but we are on the right path. We just need to
    apply the mappings to the different transactions that we now parse correctly.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,73 @@
-use calamine::{open_workbook, open_workbook_auto, DataType, Range, Reader, Xls};
+use calamine::{
+    open_workbook, open_workbook_auto, DataType, Range, RangeDeserializerBuilder, Reader, Xls,
+};
+use chrono::{DateTime, NaiveDate, Utc};
+use serde::{Deserialize, Serialize};
 use std::{
+    collections::HashMap,
     env,
     fs::File,
-    io::{self, BufWriter, Error, Result, Write},
+    io::{self, BufReader, BufWriter, Error, Result, Write},
     path::PathBuf,
 };
 
 const PATH: &str = "/tmp/export2022819.xls";
 const SHEET_NAME: &str = "Movimientos";
+const DATE_FORMAT: &str = "%d/%m/%Y";
+const CONFIG_FILE: &str = "/Users/enrikes/.config/santander_ledger.json";
+const HEADERS: &[&str] = &[
+    "fecha_operacion",
+    "fecha_valor",
+    "concepto",
+    "importe",
+    "saldo",
+];
+
+mod date_serde {
+    use chrono::NaiveDate;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    use crate::DATE_FORMAT;
+
+    pub fn serialize<S>(date: &Option<NaiveDate>, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if let Some(ref d) = *date {
+            return s.serialize_str(&d.format(DATE_FORMAT).to_string());
+        }
+        s.serialize_none()
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<NaiveDate>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let data_type = calamine::DataType::deserialize(deserializer);
+        Ok(data_type?.as_date())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Transaction {
+    #[serde(with = "date_serde")]
+    fecha_operacion: Option<NaiveDate>,
+    #[serde(with = "date_serde")]
+    fecha_valor: Option<NaiveDate>,
+    concepto: String,
+    importe: isize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    mappings: HashMap<String, String>,
+}
 
 fn skip_to_header_row(
     range: Range<DataType>,
     expected_headers: Vec<&str>,
 ) -> Result<Range<DataType>> {
     if let Some((ix, _)) = range.rows().enumerate().find(|(i, row)| {
-        println!("i: {}", i);
-        println!("row: {:?}", row);
         expected_headers
             .iter()
             .all(|h| row.iter().any(|cell: &DataType| &cell.to_string() == h))
@@ -42,71 +94,25 @@ fn skip_rows(range: Range<DataType>, n: u32) -> Result<Range<DataType>> {
     Ok(range.range((start.0 + n, start.1), end))
 }
 
-fn main() {
-    // let mut workbook: Xls<_> = open_workbook(PATH).expect("Cannot open file");
-    // if let Some(Ok(transactions)) = workbook.worksheet_range(SHEET_NAME) {
-    //     let transactions_skipped = skip_rows(transactions, 8).expect("should work");
-    //     println!(
-    //         "Transactions with skipped rows: {:?}",
-    //         transactions_skipped.rows()
-    //     );
-    //     // let rows = movimientos.rows();
+fn compute_transactions(path: &str, config: &Config) {
+    println!("{:?}", config);
+    let mut workbook: Xls<_> = open_workbook(path).expect("Cannot open file");
+    if let Some(Ok(worksheet)) = workbook.worksheet_range(SHEET_NAME) {
+        let transactions_skipped =
+            skip_to_header_row(worksheet, HEADERS.to_vec()).expect("should work");
+        let transactions: Vec<Transaction> = RangeDeserializerBuilder::with_headers(HEADERS)
+            .from_range::<_, Transaction>(&transactions_skipped)
+            .expect("Deserializer should work.")
+            .map(|transaction| transaction.unwrap())
+            .collect();
 
-    //     // let num_rows = rows.len();
-    //     // for (i, row) in rows.enumerate() {
-    //     //     println!("{}:row={:?}", i, row);
-    //     // }
-    // }
-    // // converts first argument into a csv (same name, silently overrides
-    // // if the file already exists
-
-    let file = env::args()
-        .nth(1)
-        .expect("Please provide an excel file to convert");
-    let sheet = env::args()
-        .nth(2)
-        .expect("Expecting a sheet name as second argument");
-
-    let sce = PathBuf::from(file);
-    match sce.extension().and_then(|s| s.to_str()) {
-        Some("xlsx") | Some("xlsm") | Some("xlsb") | Some("xls") => (),
-        _ => panic!("Expecting an excel file"),
+        println!("{:?}", transactions);
     }
-
-    let dest = sce.with_extension("csv");
-    let mut dest = BufWriter::new(File::create(dest).unwrap());
-    let mut xl = open_workbook_auto(&sce).unwrap();
-    let range = xl.worksheet_range(&sheet).unwrap().unwrap();
-
-    write_range(&mut dest, &range).unwrap();
-
-    let mut workbook = open_workbook_auto(&sce.with_extension("csv")).unwrap();
-    let range = workbook.worksheet_range(&sheet).unwrap().unwrap();
-    let transactions_skipped = skip_rows(range, 8).expect("should work");
-
-    println!(
-        "Transactions with skipped rows: {:?}",
-        transactions_skipped.rows()
-    );
 }
 
-fn write_range<W: Write>(dest: &mut W, range: &Range<DataType>) -> std::io::Result<()> {
-    let n = range.get_size().1 - 1;
-    for r in range.rows() {
-        for (i, c) in r.iter().enumerate() {
-            match *c {
-                DataType::Empty => Ok(()),
-                DataType::String(ref s) => write!(dest, "{}", s),
-                DataType::Float(ref f) | DataType::DateTime(ref f) => write!(dest, "{}", f),
-                DataType::Int(ref i) => write!(dest, "{}", i),
-                DataType::Error(ref e) => write!(dest, "{:?}", e),
-                DataType::Bool(ref b) => write!(dest, "{}", b),
-            }?;
-            if i != n {
-                write!(dest, ";")?;
-            }
-        }
-        write!(dest, "\r\n")?;
-    }
-    Ok(())
+fn main() {
+    let file = File::open(CONFIG_FILE).unwrap();
+    let reader = BufReader::new(file);
+    let config: Config = serde_json::from_reader(reader).unwrap();
+    compute_transactions(PATH, &config);
 }
